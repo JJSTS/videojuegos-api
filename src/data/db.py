5704119f6a7e8d7ca2d537a8d029cd4e422131d2
2daw <juanjesustrejosalinas06@gmail.com>
2026-02-04 19:19:11 +0100

    Creación del docker con PostgreSQL

--- src/data/db.py
+++ src/data/db.py
@@ -1,6 +1,7 @@
+from unittest import result
 from dotenv import load_dotenv
 from models.videojuego import Videojuego
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import create_engine, SQLModel, Session, text, select
 import os
 
 load_dotenv()
@@ -8,22 +9,24 @@ load_dotenv()
 db_user: str = os.getenv("DB_USER","jjsts")
 db_password: str = os.getenv("DB_PASSWORD","1234")
 db_server: str = os.getenv("DB_SERVER","fastapi-db")
-db_port: int = os.getenv("DB_PORT", 3306)
+db_port: int = os.getenv("DB_PORT", 5432)
 db_name: str = os.getenv("DB_NAME","videojuegosdb")
 
-DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
-engine = create_engine(os.getenv("DB_URL",DATABASE_URL), echo=True)
+DATABASE_URL = f"postgresql+psycopg://{db_user}:{db_password}@{db_server}:{db_port}/{db_name}"
+engine = create_engine(os.getenv("DB_URL", DATABASE_URL), echo=True)
 
 def get_session():
     with Session(engine) as session:
         yield session
 
 def init_db():
-    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.drop_all(engine) 
     SQLModel.metadata.create_all(engine)
     with Session(engine) as session:
         session.add(Videojuego(id=1, nombre="The Legend of Zelda: Breath of the Wild", fecha_lanzamiento="2017-03-03", genero="Action-adventure", plataforma="Nintendo Switch"))
         session.add(Videojuego(id=2, nombre="Marvel Rivals", fecha_lanzamiento="2024-11-27", genero="Fighting", plataforma="PC"))
         session.add(Videojuego(id=3, nombre="Minecraft", fecha_lanzamiento="2011-11-18", genero="Sandbox", plataforma="PC"))
         session.add(Videojuego(id=4, nombre="Among Us", fecha_lanzamiento="2018-06-15", genero="Party", plataforma="PC/Mobile"))
+        session.commit()
+        session.execute(text("SELECT setval('videojuego_id_seq', COALESCE((SELECT MAX(id) FROM videojuego), 1), true)"))
         session.commit()
